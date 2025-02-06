82ab2e65a130b07b09e944904dc27e56ddf56745
Tara Lukose <taraann@karunya.edu.in>
2025-02-06 18:37:59 +0530

    Follow up on previous commit

--- we4us/src/App.tsx
+++ we4us/src/App.tsx
@@ -33,5 +33,37 @@ const App: React.FC = () => {
     </Router>
   );
 };
+import { useEffect, useState } from 'react'
+import { PostView } from 'lemmy-js-client';
+import { getPostList } from './components/lib';
+import PostList from './components/PostList';
+import './App.css'
+
+const Loader = () => <h3>Loading...</h3>;
+
+function App() {
+  const [postViews, setPostViews] = useState<PostView[] | null>(null)
+  useEffect(() => {
+    setTimeout(() => { // simulating a delay. TODO: Remove timeout
+      getPostList().then(postList => setPostViews(postList));
+      console.log("Fetched posts")
+    }, 1000)
+  }
+    , [])
+  if (!postViews) {
+    return <Loader />;
+  }
+  else if (postViews.length == 0) {
+    return <h3>No posts to see!</h3>;
+  }
+  else {
+    return (
+      <>
+        <h1>Recent Posts</h1>
+        <PostList postViews={postViews} />
+      </>
+    );
+  }
+}
 
 export default App;
