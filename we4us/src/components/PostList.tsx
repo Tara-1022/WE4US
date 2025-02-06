49d46b9827ac8a96e3d3383578bee479871cbcad
Tara Lukose <taraann@karunya.edu.in>
2025-02-06 20:29:06 +0530

    Rename 'Post' to 'PostSnippet' to better reflect intent. Improve styling.

--- we4us/src/components/PostList.tsx
+++ we4us/src/components/PostList.tsx
@@ -1,4 +1,4 @@
-import Post from "./Post";
+import PostSnippet from "./PostSnippet";
 import { PostView } from 'lemmy-js-client';
 
 export default function PostList({ postViews }: { postViews: PostView[] }) {
@@ -14,7 +14,7 @@ export default function PostList({ postViews }: { postViews: PostView[] }) {
     }
     const list = postViews.map(
         postView => <li key={postView.post.id} style={styles.listItem}>
-                        <Post postView={postView} />
+                        <PostSnippet postView={postView} />
                     </li>
     );
     return <ul style={styles.list}>{list}</ul>;
---
+++ we4us/src/components/PostSnippet.tsx
@@ -0,0 +1,42 @@
+import { PostView } from 'lemmy-js-client';
+import default_image from '../assets/default_image.png'
+
+export default function PostSnippet({postView}: {postView: PostView}){
+    let styles: {[key: string]: React.CSSProperties } = {
+        post: {
+            display: "flex",
+            flexFlow: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 10
+        },
+        imageContainer: {
+            flex: 1,
+            aspectRatio: "1",
+            overflow: "hidden",
+        },
+        image: {
+            width: "100%",
+            height: "100%",
+            objectFit: "cover"
+        },
+        details: {
+            flex: 4
+        }
+    }
+    return (
+        <div style={styles.post}>
+            <a href='/sample_post' style={styles.imageContainer}>
+                <img 
+                src={postView.image_details? postView.image_details.link : default_image} 
+                alt="PostImage"
+                style={styles.image} />
+            </a>
+            <div style={styles.details}>
+                <h3>{postView.post.name}</h3>
+                <p>{postView.creator.display_name?postView.creator.display_name:postView.creator.name}</p>
+                <p>{postView.community.name}</p>
+            </div>
+        </div>
+    );
+}
