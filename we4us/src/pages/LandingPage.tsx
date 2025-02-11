b8d0b042dd4bd8efdf458e3f5c1224d192bc6c8c
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-02-11 18:47:03 +0530

    Move feed to the appropriate page

--- we4us/src/pages/LandingPage.tsx
+++ we4us/src/pages/LandingPage.tsx
@@ -1,33 +1,7 @@
 import React from 'react';
-import { useEffect, useState } from 'react'
-import { PostView } from 'lemmy-js-client';
-import { getPostList } from '../library/LemmyApi';
-import PostList from '../components/PostList';
-import { Loader } from 'lucide-react';
 
 const LandingPage: React.FC = () => {
-  const [postViews, setPostViews] = useState<PostView[] | null>(null)
-  useEffect(() => {
-    setTimeout(() => { // simulating a delay. TODO: Remove timeout
-      getPostList().then(postList => setPostViews(postList));
-      console.log("Fetched posts")
-    }, 1000)
-  }
-    , [])
-  if (!postViews) {
-    return <Loader />;
-  }
-  else if (postViews.length == 0) {
-    return <h3>No posts to see!</h3>;
-  }
-  else {
-    return (
-      <>
-        <h1>Recent Posts</h1>
-        <PostList postViews={postViews} />
-      </>
-    );
-  }
+  return <h3>Landing Page</h3>
 }
 
 export default LandingPage;
