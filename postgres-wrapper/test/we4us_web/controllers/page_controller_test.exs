d4b79f22fecf7acb28c3301b58ff60f8b7c186c5
Nishita <nishitapravin2@gmail.com>
2025-02-25 11:12:30 +0530

    Push .exs file

---
+++ postgres-wrapper/test/we4us_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule We4usWeb.PageControllerTest do
+  use We4usWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
