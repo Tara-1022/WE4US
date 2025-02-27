0cc999d93222899271c642e4cf7cd7d04cc64010
Nishita <nishitapravin2@gmail.com>
2025-02-27 17:41:42 +0530

    Extract API URLs and fetch logic into api.tsx

---
+++ we4us/src/api.tsx
@@ -0,0 +1,40 @@
+import { API_BASE_URL, PROFILES_ENDPOINT } from "./constants";
+
+export const fetchProfiles = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}${PROFILES_ENDPOINT}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch profiles");
+    }
+    const jsonData = await response.json();
+
+    return jsonData.profiles.map((p: any) => ({
+      id: p.id,
+      username: p.username,
+      displayName: p.display_name, 
+      cohort: p.cohort,
+    }));
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
+export const fetchProfileById = async (id: number) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}${PROFILES_ENDPOINT}${id}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch profile");
+    }
+    return await response.json();
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
--- we4us/src/constants.tsx
+++ we4us/src/constants.tsx
@@ -1,1 +1,3 @@
 export const INSTANCE_URL = "https://enterprise.lemmy.ml/"
+export const API_BASE_URL = "http://localhost:4000/api/";
+export const PROFILES_ENDPOINT = "profiles/";
