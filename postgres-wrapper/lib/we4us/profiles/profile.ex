f7b34826ea0223e3dc0be2db105611c8cce99f0f
nishitaKatepallewar <nishitapravin2@gmail.com>
2025-03-20 17:51:58 +0530

    Change Primary Key to 'username' in Profiles (#48)
    * Refactor: Change primary key of profiles from id to username
    
    * Ensure API accepts username and that username is unique and validates fomrat

--- postgres-wrapper/lib/we4us/profiles/profile.ex
+++ postgres-wrapper/lib/we4us/profiles/profile.ex
@@ -2,17 +2,13 @@ defmodule We4us.Profiles.Profile do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:username, :string, autogenerate: false}
   #Prevents Ecto metadat from causing encoding errors
-  @derive {Jason.Encoder, only: [:id, :display_name, :username, :cohort, :current_role, :company_or_university, :years_of_experience, :areas_of_interest, :inserted_at, :updated_at]}
+  @derive {Jason.Encoder, only: [:username, :display_name, :cohort, :current_role, :company_or_university, :years_of_experience, :areas_of_interest, :inserted_at, :updated_at]}
 
   schema "profiles" do
     field :cohort, :string
-    # field :comments, :integer, default: 0
     field :display_name, :string
-    # field :join_date, :date, default: Date.utc_today()
-    # field :posts, :integer, default: 0
-    field :username, :string
-
     field :current_role, :string
     field :company_or_university, :string
     field :years_of_experience, :integer, default: 0
@@ -26,8 +22,10 @@ defmodule We4us.Profiles.Profile do
     profile
     |> cast(attrs, [:display_name, :username, :cohort, :current_role, :company_or_university, :years_of_experience, :areas_of_interest])
     |> validate_required([:display_name, :username])  # cohort remains optional
-    |> validate_length(:display_name, min: 2, max: 100)
-    |> validate_length(:username, min: 2, max: 50)
+    |> validate_length(:display_name, min: 3, max: 100)
+    |> validate_length(:username, min: 3, max: 50)
     |> validate_number(:years_of_experience, greater_than_or_equal_to: 0)
+    |> validate_format(:username, ~r/^[a-z0-9_]+$/, message: "must be lowercase, no spaces, and use underscores")
+    |> unique_constraint(:username, message: "Username is already taken")
   end
 end
