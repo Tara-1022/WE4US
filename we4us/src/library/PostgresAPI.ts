62e7a030ef64665265dc5316abe2bdb9c3028254
Nishita <nishitapravin2@gmail.com>
2025-03-20 21:40:17 +0530

    Remove instances of id in profile API calls

--- we4us/src/library/PostgresAPI.ts
+++ we4us/src/library/PostgresAPI.ts
@@ -1,7 +1,6 @@
 import { API_BASE_URL, PROFILES_ENDPOINT } from "../constants";
 
 export interface Profile {
-  id: string;
   username: string;
   display_name: string;
   cohort?: string;
@@ -20,29 +19,12 @@ export const fetchProfiles = async () => {
     const jsonData = await response.json();
 
     return jsonData.profiles.map((p: any) => ({
-      id: p.id,
       username: p.username,
       displayName: p.display_name, 
       cohort: p.cohort,
       companyOrUniversity: p.company_or_university,
       currentRole: p.current_role,
     }));
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    } else {
-      throw new Error("Unknown error occurred.");
-    }
-  }
-};
-
-export const fetchProfileById = async (id: number) => {
-  try {
-    const response = await fetch(`${API_BASE_URL}${PROFILES_ENDPOINT}/${id}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch profile");
-    }
-    return await response.json();
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
@@ -68,7 +50,6 @@ export const fetchProfileByUsername = async (username: string) => {
     }
 
     return {
-      id: profile.id,
       username: profile.username,
       display_name: profile.display_name, 
       cohort: profile.cohort,
