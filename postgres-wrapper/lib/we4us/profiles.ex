7d969097e2a7a12d30bb29a92e71f1a1f473c59b
Nishita <nishitapravin2@gmail.com>
2025-03-20 17:37:29 +0530

    Remove redundant username formatting and error handling

--- postgres-wrapper/lib/we4us/profiles.ex
+++ postgres-wrapper/lib/we4us/profiles.ex
@@ -24,16 +24,12 @@ defmodule We4us.Profiles do
     # Ensure username is present
     case Map.fetch(attrs, "username") do
       {:ok, username} ->
-        username = String.downcase(username) |> String.replace(" ", "_")  # Enforce format
 
         attrs = Map.put(attrs, "username", username)
 
         %Profile{}
         |> Profile.changeset(attrs)
         |> Repo.insert()
-
-      :error ->
-        {:error, "Username is required"}  # Explicit error if missing
     end
   end
 
