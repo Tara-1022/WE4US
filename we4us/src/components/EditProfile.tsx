bfa2d0bd2d77a09aca02f7996214a6fd948b5282
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-03-20 21:49:58 +0530

    Disable username edits and add display name regex.

--- we4us/src/components/EditProfile.tsx
+++ we4us/src/components/EditProfile.tsx
@@ -26,22 +26,37 @@ const ProfileEditForm = ({ profile, onProfileUpdate, onCancel }: ProfileEditForm
       </>
     )
   }
+
+  function validateDisplayName(displayName: string) {
+    // https://github.com/LemmyNet/lemmy/blob/d993f6cff7804f0b07b5f76c31f4efb29860f7ba/crates/utils/src/utils/validation.rs#L128C8-L128C29
+    const displayNameRegex = /^[A-Za-z0-9 _\.,!\?;:\\\-\<\>\(\)\#\$\%\^\*&]+$/;
+    if (!displayNameRegex.test(displayName)) {
+      setError("Display name must not have special characters, invisble whitespaces, or newline");
+      return false;
+    }
+    return true;
+  }
   
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsProcessing(true);
     setError(null);
     const formData = new FormData(e.currentTarget);
-    const { display_name, username, cohort, current_role, company_or_university, years_of_experience, areas_of_interest }
+    const { display_name, cohort, current_role, company_or_university, years_of_experience, areas_of_interest }
       = Object.fromEntries(formData);
 
     const areas: string[] = areas_of_interest.toString().split(",").map((area: string) => area.trim());
+
+    if (!validateDisplayName(display_name.toString())) {
+      setIsProcessing(false);
+      return;
+    }
     
     try {
       const response = await updateProfile(profile.username, {
         id: profile.id,
+        username: profile.username,
         display_name: display_name.toString(),
-        username: username.toString(),
         cohort: cohort?.toString() || "",
         current_role: current_role?.toString() || "",
         company_or_university: company_or_university?.toString() || "",
@@ -71,15 +86,8 @@ const ProfileEditForm = ({ profile, onProfileUpdate, onCancel }: ProfileEditForm
             type="text"
             name="display_name"
             defaultValue={profile.display_name || ''}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            name="username"
-            defaultValue={profile.username || ''}
+            minLength={3}
+            maxLength={20}
             required
           />
         </div>
