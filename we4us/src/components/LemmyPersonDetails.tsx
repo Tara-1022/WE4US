b9881e18413fef9a251643f16abed758c854cbc9
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-04-02 19:56:16 +0530

    Better error handling - log error & display error message

--- we4us/src/components/LemmyPersonDetails.tsx
+++ we4us/src/components/LemmyPersonDetails.tsx
@@ -4,7 +4,7 @@ import { getPersonDetails } from "../library/LemmyApi";
 import { CommentView, PostView } from "lemmy-js-client";
 import PostList from "./PostList";
 import CommentList from "./CommentList";
-import { ToggleLeft, ToggleRight } from "lucide-react";
+import { Loader, ToggleLeft, ToggleRight } from "lucide-react";
 
 type OverviewDetails = {
     isAdmin: boolean,
@@ -12,28 +12,50 @@ type OverviewDetails = {
     commentsCount: number
 }
 
+type PersonDetails = {
+    posts: PostView[],
+    comments: CommentView[],
+    overview: OverviewDetails
+}
+
 export default function LemmyPersonDetails({ username }: { username: string }) {
-    const [posts, setPosts] = useState<PostView[]>([]);
-    const [comments, setComments] = useState<CommentView[]>([]);
-    const [overview, setOverview] = useState<OverviewDetails>();
+    const [personDetails, setPersonDetails] = useState<PersonDetails>();
+    const [isLoading, setLoading] = useState<boolean>(false);
     const [isPostsToggle, setPostsToggle] = useState<boolean>(true);
 
     useEffect(
         () => {
+            setLoading(true);
             getPersonDetails(username)
                 .then(
                     (personDetails) => {
-                        setPosts(personDetails.posts);
-                        setComments(personDetails.comments);
-                        setOverview({
-                            isAdmin: personDetails.person_view.is_admin,
-                            postsCount: personDetails.person_view.counts.post_count,
-                            commentsCount: personDetails.person_view.counts.comment_count
+                        setPersonDetails({
+                            posts: personDetails.posts,
+                            comments: personDetails.comments,
+                            overview: {
+                                isAdmin: personDetails.person_view.is_admin,
+                                postsCount: personDetails.person_view.counts.post_count,
+                                commentsCount: personDetails.person_view.counts.comment_count
+                            }
                         });
                     }
                 )
+                .catch(
+                    (error) => console.error("Error fetching person details:", error)
+                )
+                .finally(() => setLoading(false))
         }, [username]
     )
+
+    if (isLoading) return <Loader />;
+
+    if (!personDetails) {
+        return <div className="profile-details" style={{ textAlign: "left" }}>
+            <h5 style={{ color: "red" }}>
+                Could not fetch Reaching out profile!
+            </h5>
+        </div>
+    }
 
     return (
         <>
@@ -42,15 +64,15 @@ export default function LemmyPersonDetails({ username }: { username: string }) {
                     Reaching out Profile:
                 </h3>
                 <div className="detail-item">
-                    <span className="detail-value"><b>{overview?.isAdmin && "Is an Admin!"}</b></span>
+                    <span className="detail-value"><b>{personDetails.overview.isAdmin && "Is an Admin!"}</b></span>
                 </div>
                 <div className="detail-item">
                     <span className="detail-label">Posts: </span>
-                    <span className="detail-value">{overview?.postsCount || 0}</span>
+                    <span className="detail-value">{personDetails.overview.postsCount || 0}</span>
                 </div>
                 <div className="detail-item">
                     <span className="detail-label">Comments: </span>
-                    <span className="detail-value">{overview?.commentsCount || 0}</span>
+                    <span className="detail-value">{personDetails.overview.commentsCount || 0}</span>
                 </div>
             </div>
 
@@ -66,9 +88,9 @@ export default function LemmyPersonDetails({ username }: { username: string }) {
                 <span style={{ margin: "0 10px" }}>Comments</span>
                 {
                     isPostsToggle ?
-                        posts.length > 0 ? <PostList postViews={posts} /> : <><br/><h4>No posts yet!</h4></>
+                        personDetails.posts.length > 0 ? <PostList postViews={personDetails.posts} /> : <><br /><h4>No posts yet!</h4></>
                         :
-                        comments.length > 0 ? <CommentList commentViews={comments} /> : <><br/><h4>No comments yet!</h4></>
+                        personDetails.comments.length > 0 ? <CommentList commentViews={personDetails.comments} /> : <><br /><h4>No comments yet!</h4></>
                 }
             </div>
         </>
