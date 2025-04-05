8060d124a88579739ed6599fb38e3224a5951df8
kajal-jotwani <kajaljotwani06@gmail.com>
2025-04-05 19:39:36 +0530

    Landing page styles added

--- we4us/src/pages/LandingPage.tsx
+++ we4us/src/pages/LandingPage.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import AnnouncementsPreview from '../components/Announcements/AnnouncementsPreview';
-import "../components/Announcements/AnnouncementsPreview.css"
+import { GraduationCap } from 'lucide-react';
+import WE from "../assets/We.png";
+import '../styles/LandingPage.css'; 
+import MemoriesGallery from '../components/MemoriesGallery';
+import { memories } from '../components/MemoriesData'; 
+
 const LandingPage: React.FC = () => {
   return (
     <>
-      <h1>Landing Page</h1>
-      <AnnouncementsPreview />
-    </>
-  )
-}
+    <div className="landing-container">
+      <div className="landing-left">
+        <div>
+          <div className="landing-title">
+            <GraduationCap size={70} />
+            <h1>WE4US</h1>
+          </div>
+          <p className="landing-desc">
+            Hello Duckie! Welcome to WE4US, a vibrant community of smart women engineers shaping 
+            the future of technology.
+          </p>
+            <button className="landing-btn">
+              Signin
+            </button>
+        </div>
+      </div>
+      <div className="landing-right">
+        <img src={WE} alt="Women in Tech" className="landing-image" />
+      </div>
+    </div>
+    <div className="memories-section">
+    <MemoriesGallery memories={memories} />
+  </div>
+  </>
+  );
+};
 
 export default LandingPage;
