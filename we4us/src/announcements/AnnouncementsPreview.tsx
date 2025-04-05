08bf84a29dc192f4959f542d08a9f273ee8631c6
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-04-05 14:29:29 +0530

    Add preview of 5 recent announcements to the landing page

---
+++ we4us/src/announcements/AnnouncementsPreview.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { PostView } from "lemmy-js-client";
+import AnnouncementPostSnippet from "../announcements/AnnouncementPostSnippet";
+import { Loader } from 'lucide-react';
+import { getAnnouncementPostList } from "../library/LemmyApi";
+
+export default function AnnouncementsPreview() {
+    const [postViews, setPostViews] = useState<PostView[] | null>(null);
+
+    useEffect(() => {
+        const loadPosts = async () => {
+            const posts = await getAnnouncementPostList(5);
+            setPostViews(posts);
+        };
+
+        loadPosts();
+    }, []);
+
+
+    if (!postViews) return <Loader />;
+    else {
+        const list = postViews.map(
+            postView => <li key={postView.post.id} className="announcement-snippet">
+                <AnnouncementPostSnippet postView={postView} />
+            </li>
+        );
+        return (
+            <div className="announcements-preview-container">
+                <ul className="announcement-list">{list}</ul>
+            </div>
+        );
+    }
+}
--- we4us/src/pages/LandingPage.tsx
+++ we4us/src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import AnnouncementsPreview from '../announcements/AnnouncementsPreview';
+import "../announcements/AnnouncementsPreview.css"
 const LandingPage: React.FC = () => {
   return (
     <>
       <h1>Landing Page</h1>
+      <AnnouncementsPreview />
     </>
   )
 }
