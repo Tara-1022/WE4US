6ccecba556bc9fae1eccca0d101d4f2d226f06ef
kajal-jotwani <kajaljotwani06@gmail.com>
2025-04-18 12:15:33 +0530

    Updated the memories gallery

--- we4us/src/assets/memories/MemoriesData.ts
+++ we4us/src/assets/memories/MemoriesData.ts
@@ -1,6 +1,22 @@
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
+import C5_img1 from "./C5_img1.jpeg";
+import C5_img2 from "./C5_img2.jpeg";
+import C5_img3 from "./C5_img3.jpg";
+import C5_img4 from "./C5_img4.jpeg";
+import C5_img5 from "./C5_img5.jpeg";
+import Alumni1 from './Alumni_fest1.jpg'
+import Alumni2 from './Alumni_fest2.jpg'
+import Alumni3 from './Alumni_fest3.jpg'
+import C4_img1 from './C4_img1.jpg'
+import C4_img2 from './C4_img2.jpg'
+import C4_img3 from './C4_img3.jpg'
+import C2_img1 from './C2_img1.jpeg'
+import C2_img2 from './C2-img2.jpeg'
+import C2_img3 from './C2_img3.jpeg'
+import Christmas_img from './Christmas C4.jpg'
+import Graduation_img from './Graduation C4.jpg'
 
 export const memories = [
     {
@@ -17,6 +33,86 @@ export const memories = [
       imageUrl: img2,
       title: "Bootcamp Bash",
       description: "Celebrating the end of a successful bootcamp.",
+    },
+    {
+      imageUrl: C5_img1,
+      title: "C5",
+      description: "",
+    },
+    {
+      imageUrl: C5_img2,
+      title: "C5",
+      description: "",
+    },
+    {
+      imageUrl: C5_img3,
+      title: "C5",
+      description: "",
+    },
+    {
+      imageUrl: C5_img4,
+      title: "C5",
+      description: "",
+    },
+    {
+      imageUrl: C5_img5,
+      title: "C5",
+      description: "",
+    },
+    {
+      imageUrl: Alumni1,
+      title: "",
+      description: "",
+    },
+    {
+      imageUrl: Alumni2,
+      title: "",
+      description: "",
+    },
+    {
+      imageUrl: Alumni3,
+      title: "",
+      description: "",
+    },
+    {
+      imageUrl: C4_img1,
+      title: "C4",
+      description: "",
+    },
+    {
+      imageUrl: C4_img2,
+      title: "C4",
+      description: "",
+    },
+    {
+      imageUrl: C4_img3,
+      title: "C4",
+      description: "",
+    },
+    {
+      imageUrl: C2_img1,
+      title: "C2",
+      description: "",
+    },
+    {
+      imageUrl: C2_img2,
+      title: "C2",
+      description: "",
+    },
+    {
+      imageUrl: C2_img3,
+      title: "C2",
+      description: "",
+    },
+    {
+      imageUrl: Christmas_img,
+      title: "C4 Christmas Party",
+      description: "Celebrating the festive season with joy and laughter.",
+    },
+    {
+      imageUrl: Graduation_img,
+      title: "C4 Graduation",
+      description: "Celebrating the end of a successful programme.",
     },
   ];
   
