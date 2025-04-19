7d63439b01ac101660da59368661687a6f72991a
Khushi-S-29 <khushiumalakshmi@gmail.com>
2025-04-19 16:49:25 +0530

    added search feature

--- we4us/src/pages/MeetUpPage.tsx
+++ we4us/src/pages/MeetUpPage.tsx
@@ -19,11 +19,17 @@ export default function MeetUpPage() {
     return (
         <div className="meetup-page">
           <div className="meetup-header-row">
-            <h2 className="meetup-header">Meet Up</h2>
-            <PostCreationHandler
-              handleCreatedPost={(newPost) => setPostViews([newPost, ...(postViews || [])])}
-            />
-          </div>
+  <h2 className="meetup-header">Meet Up</h2>
+  <div className="meetup-header-actions">
+    <Link to="/meetup/search">
+      <Search className="meetup-search-icon" />
+    </Link>
+    <PostCreationHandler
+      handleCreatedPost={(newPost) => setPostViews([newPost, ...(postViews || [])])}
+    />
+  </div>
+</div>
+
       
           {postViews.length === 0 ? (
             <h3 className="no-posts">No posts to see!</h3>
