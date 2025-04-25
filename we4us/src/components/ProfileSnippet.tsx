f277a0e25776fc5ff81b12d8b38d4c0b754bcef8
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-04-25 10:11:02 +0530

    Add carousel to who's who

--- we4us/src/components/ProfileSnippet.tsx
+++ we4us/src/components/ProfileSnippet.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { constructImageUrl } from '../library/ImageHandling';
 import DuckAvatar from '../assets/profile_duck.png';
-import "./ProfileSnippet.css";
+import "../styles/ProfileSnippet.css";
+import { formatToN } from "../library/Utils";
 import { Profile } from "../library/PostgresAPI";
 
 interface ProfileSnippetProps {
@@ -10,7 +11,7 @@ interface ProfileSnippetProps {
 }
 
 const ProfileSnippet: React.FC<ProfileSnippetProps> = ({ profile }) => {
-  const { username, display_name, image_filename, cohort } = profile;
+  const { username, display_name, image_filename } = profile;
 
   const profileImageUrl = image_filename
     ? constructImageUrl(image_filename)
@@ -19,9 +20,9 @@ const ProfileSnippet: React.FC<ProfileSnippetProps> = ({ profile }) => {
   return (
     <Link to={`/profile/${username}`} className="profile-snippet">
       <div>
-        <img src={profileImageUrl} alt={display_name} className="profile-w-image" />
-        <h3>{display_name}</h3>
-        <p>@{username}</p>
+        <img src={profileImageUrl} alt={display_name} className="profile-image" />
+        <h3 className="display-name">{formatToN(display_name, 15)}</h3>
+        <p className="username">@{username}</p>
       </div>
     </Link>
   );
