75971a361576cc5b99ac097356e10df3edf329aa
Dhenuvakonda Saathwika <118562491+saathwikad@users.noreply.github.com>
2025-04-26 11:30:19 +0530

    Pagination for Job Board, Meet Up and PG Finder Features. (#138)
    * Added Pagination to the Feature Pages.
    
    * Added Pagination to the Feature Pages.

--- we4us/src/pages/MeetUpPage.tsx
+++ we4us/src/pages/MeetUpPage.tsx
@@ -5,38 +5,43 @@ import { Loader, Search } from "lucide-react";
 import { Link } from "react-router-dom";
 import { getMeetUpPostList } from "../library/LemmyApi";
 import PostCreationHandler from "../components/MeetUp/PostCreationHandler";
+import PaginationControls from "../components/PaginationControls";
+import { DEFAULT_POSTS_PER_PAGE } from "../constants";
 import "../styles/MeetUpPage.css";
 
 export default function MeetUpPage() {
-    const [postViews, setPostViews] = useState<PostView[] | null>(null);
+    const [postViews, setPostViews] = useState<PostView[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const hasMore = postViews.length >= DEFAULT_POSTS_PER_PAGE;
 
     useEffect(() => {
-        getMeetUpPostList().then(setPostViews);
-    }, []);
-
+        getMeetUpPostList(page).then(setPostViews);
+    }, [page]);
     if (!postViews) return <Loader />;
 
     return (
         <div className="meetup-page">
-          <div className="meetup-header-row">
-  <h2 className="meetup-header">Meet Up</h2>
-  <div className="meetup-header-actions">
-    <Link to="/meetup/search">
-      <Search className="meetup-search-icon" />
-    </Link>
-    <PostCreationHandler
-      handleCreatedPost={(newPost) => setPostViews([newPost, ...(postViews || [])])}
-    />
-  </div>
-</div>
+            <div className="meetup-header-row">
+                <h2 className="meetup-header">Meet Up</h2>
+                <div className="meetup-header-actions">
+                    <Link to="/meetup/search">
+                        <Search className="meetup-search-icon" />
+                    </Link>
+                    <PostCreationHandler
+                        handleCreatedPost={(newPost) => setPostViews([newPost, ...(postViews || [])])}
+                    />
+                </div>
+            </div>
 
-      
-          {postViews.length === 0 ? (
-            <h3 className="no-posts">No posts to see!</h3>
-          ) : (
-            <MeetUpPostList postViews={postViews} />
-          )}
+            <PaginationControls page={page} setPage={setPage} hasMore={hasMore} />
+
+            {postViews.length === 0 ? (
+                <h3 className="no-posts">No posts to see!</h3>
+            ) : (
+                <MeetUpPostList postViews={postViews} />
+            )}
+            <PaginationControls page={page} setPage={setPage} hasMore={hasMore} />
         </div>
-      );
-      
+    );
+    
 }
