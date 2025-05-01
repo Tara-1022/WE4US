43109ecbde9182c3f59ecd1387681bbc8d824c89
kajal-jotwani <kajaljotwani06@gmail.com>
2025-05-01 19:06:52 +0530

    Updated styles for buttons

--- we4us/src/pages/AnnouncementPostPage.tsx
+++ we4us/src/pages/AnnouncementPostPage.tsx
@@ -1,24 +1,14 @@
 import { PostView } from 'lemmy-js-client';
 import { useEffect, useState } from 'react';
 import { getPostById } from '../library/LemmyApi';
-import { Loader } from 'lucide-react';
+import { Loader, Bell, Edit } from 'lucide-react';
 import { useParams, Link } from 'react-router-dom';
 import CommentsSection from '../components/CommentsSection';
 import ReactMarkdown from "react-markdown";
 import PostDeletor from '../components/PostDeletor';
 import AnnouncementEditor from '../components/Announcements/AnnouncementEditor';
-import { Bell } from 'lucide-react';
 import { useProfileContext } from '../components/ProfileContext';
-
-let styles: { [key: string]: React.CSSProperties } = {
-    post: {
-        margin: "10px"
-    },
-    content: {
-        margin: "10px",
-        marginLeft: "30px"
-    }
-}
+import "../styles/AnnouncementPostPage.css"
 
 export default function AnnouncementPostPage() {
     const announcementId = Number(useParams().announcementId);
@@ -31,7 +21,6 @@ export default function AnnouncementPostPage() {
             getPostById(announcementId).then(
                 response => {
                     setPostView(response ? response.post_view : null);
-                    console.log(response)
                 }
             )
         },
@@ -41,40 +30,47 @@ export default function AnnouncementPostPage() {
 
     return (
         <>
-            {isEditing ?
-                <AnnouncementEditor
-                    onClose={() => setIsEditing(false)}
-                    onPostUpdated={setPostView}
-                    postView={postView} />
-                :
+            {isEditing ? (
+                <div className="announcement-editor-container">
+                    <AnnouncementEditor
+                        onClose={() => setIsEditing(false)}
+                        onPostUpdated={setPostView}
+                        postView={postView}
+                    />
+                </div>
+            ) : (
                 <>
-                    <div style={styles.post}>
-                        <h3><Bell />&nbsp; {postView.post.name}</h3>
-                        <div style={styles.content}>
+                    <div className="announcement-post">
+                        <div className="announcement-header">
+                            <h3>
+                                <Bell className="bell-icon" />
+                                &nbsp; {postView.post.name}
+                            </h3>
+                            {postView.creator.id === profileInfo?.lemmyId && (
+                                <div className="announcement-actions">
+                                    <button 
+                                        className="icon-button" 
+                                        onClick={() => setIsEditing(true)}
+                                        title="Edit post"
+                                    >
+                                        <Edit size={18} />
+                                    </button>
+                                    <PostDeletor postId={postView.post.id} />
+                                </div>
+                            )}
+                        </div>
+                        <div className="announcement-content">
                             <ReactMarkdown>{postView.post.body}</ReactMarkdown>
                         </div>
                         <Link to={"/profile/" + postView.creator.name}>
-                            <p>
-                                - {postView.creator.display_name ? postView.creator.display_name : postView.creator.name}
+                            <p className="announcement-author">
+                                - {postView.creator.display_name || postView.creator.name}
                             </p>
                         </Link>
                     </div>
-
-                    {postView.creator.id == profileInfo?.lemmyId &&
-                        <>
-                            <PostDeletor postId={postView.post.id} />
-                            &nbsp;
-                            {!isEditing &&
-                                <b
-                                    style={{ cursor: "pointer" }}
-                                    onClick={() => setIsEditing(true)}
-                                >
-                                    Edit
-                                </b>}
-                        </>}
+                    <CommentsSection postId={postView.post.id} />
                 </>
-            }
-            <CommentsSection postId={postView.post.id} />
+            )}
         </>
     );
 }
