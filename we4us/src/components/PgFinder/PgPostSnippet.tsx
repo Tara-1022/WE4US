97f8d222c53eb39ad14afd782cc672ff513e661d
ChinnmayieN <chinnmayie.nagam@gmail.com>
2025-05-12 16:00:45 +0000

    delted the ratings comment

--- we4us/src/components/PgFinder/PgPostSnippet.tsx
+++ we4us/src/components/PgFinder/PgPostSnippet.tsx
@@ -20,12 +20,6 @@ export default function PgPostSnippet({ postView }: { postView: PostView }) {
                 <div className="pg-post-location">
                 ⚲ Location: {pgData.location || "Not specified"}
                 </div>
-                {/* <div className="pg-post-ratings">
-                    <span className="pg-post-rating-item">Cost: {pgData.ratings?.cost || "N/A"}</span>
-                    <span className="pg-post-rating-item">Safety: {pgData.ratings?.safety || "N/A"}</span>
-                    <span className="pg-post-rating-item">Food: {pgData.ratings?.food || "N/A"}</span>
-                    <span className="pg-post-rating-item">Cleanliness: {pgData.ratings?.cleanliness || "N/A"}</span>
-                </div> */}
                 </div>
         </div>
     );
