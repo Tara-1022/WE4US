62de3d4461ce76dcb0a7c247af0b368ce737cf4a
Tara Lukose <100012608+Tara-1022@users.noreply.github.com>
2025-05-28 19:09:36 +0530

    Add API to get latest message per user-user pair

--- postgres-wrapper/lib/we4us/messages.ex
+++ postgres-wrapper/lib/we4us/messages.ex
@@ -1,5 +1,6 @@
 defmodule We4us.Messages do
   import Ecto.Query, warn: false
+  alias We4us.Messages
   alias We4us.Repo
   alias We4us.Messages.Message
 
@@ -11,12 +12,45 @@ defmodule We4us.Messages do
 
   def get_conversation(user1, user2) do
     try do
-      messages = from(m in Message,
-        where: (m.from_user == ^user1 and m.to_user == ^user2) or
-               (m.from_user == ^user2 and m.to_user == ^user1),
-        order_by: [asc: m.inserted_at]
-      )
-      |> Repo.all()
+      messages =
+        from(m in Message,
+          where:
+            (m.from_user == ^user1 and m.to_user == ^user2) or
+              (m.from_user == ^user2 and m.to_user == ^user1),
+          order_by: [asc: m.inserted_at]
+        )
+        |> Repo.all()
+
+      {:ok, messages}
+    rescue
+      e ->
+        {:error, "Failed to retrieve messages: #{inspect(e)}"}
+    end
+  end
+
+  def get_last_message_per_pair(user) do
+    try do
+      select_query =
+        from(m in Message,
+          where: m.from_user == ^user or m.to_user == ^user
+        )
+
+      query =
+        from(m in subquery(select_query),
+          distinct:
+            fragment(
+              "LEAST(?, ?) || '#' || GREATEST(?, ?)",
+              m.from_user,
+              m.to_user,
+              m.from_user,
+              m.to_user
+            ),
+          order_by: [desc: m.inserted_at],
+          select: m
+        )
+
+      messages =
+        Repo.all(query)
 
       {:ok, messages}
     rescue
